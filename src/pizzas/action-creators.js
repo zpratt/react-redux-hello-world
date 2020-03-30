def12c44e90dd55273b2202d070797db54eaa1fd
zach pratt <zach@modzed.io>
2020-03-29 23:33:25 -0500

    make the action creator async and use a mock pizzas API

--- src/pizzas/action-creators.js
+++ src/pizzas/action-creators.js
@@ -1,8 +1,16 @@
 export const loadPizzas = () => {
     return (dispatch) => {
-        dispatch({
-            type: 'LOAD_PIZZAS',
-            pizzas: ['cheese', 'sausage']
-        });
+        const mockPizzasApi = 'http://www.mocky.io/v2/5e8175c43000002d006f9772';
+
+        return fetch(mockPizzasApi)
+            .then((responseStream) => {
+                return responseStream.json()
+            })
+            .then((pizzas) => {
+                return dispatch({
+                    type: 'LOAD_PIZZAS',
+                    pizzas: pizzas
+                });
+            });
     }
 };
